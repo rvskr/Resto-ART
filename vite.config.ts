8e26463ac916f1579c3b105865c7b724a6e18480
vskr <93551495+rvskr@users.noreply.github.com>
2025-02-14 04:45:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/', // Корень для локальной разработки
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
-    host: true, 
+    host: true, // Разрешает доступ извне
+    strictPort: true, // Гарантирует, что порт не изменится
+    cors: true, // Разрешает CORS для всех источников
     proxy: {
       '/api': {
-        target: 'https://resto-art.onrender.com', 
+        target: 'https://resto-art.onrender.com', // Адрес API
         changeOrigin: true,
-        secure: true, 
+        secure: true, // true, если сервер работает на HTTPS
       },
     },
   },
